33a2fc05e64209fc4a87b6c2e6a951d3c0ea702f
Gerald Annan <annang2@stanbic.com.gh>
2022-01-17 20:01:54 +0000

    Initial commit

---
+++ lib/library_fees.ex
@@ -0,0 +1,19 @@
+defmodule LibraryFees do
+  def datetime_from_string(string) do
+  end
+
+  def before_noon?(datetime) do
+  end
+
+  def return_date(checkout_datetime) do
+  end
+
+  def days_late(planned_return_date, actual_return_datetime) do
+  end
+
+  def monday?(datetime) do
+  end
+
+  def calculate_late_fee(checkout, return, rate) do
+  end
+end
